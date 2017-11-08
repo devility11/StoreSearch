69ea05c915e82b92bca4bc2250066b565b90000f
Norbert Czirjak <czirjak.norbert@gmail.com>
2017-11-08 21:01:25 +0100

    custom cells added

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,22 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Norbert Czirjak on 2017. 11. 08..
+//  Copyright © 2017. Norbert Czirjak. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
